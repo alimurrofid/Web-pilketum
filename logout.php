d36804f8ca0445c07b0c0e7bfa9907968399546a
alimurrofid <alimurrofid77@gmail.com>
2023-03-17 11:23:32 +0700

    mengawali bootstrap

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once "kon/koneksi.php";
-session_start();
-unset($_SESSION['nim']);
-session_destroy();
-header('Location: index.php');
-print_r($_SESSION['nim']);
-?>
--- welcome.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    $id = $_GET['id'];
-    $_SESSION['calonku'] = $id;
-?>
-WishList
